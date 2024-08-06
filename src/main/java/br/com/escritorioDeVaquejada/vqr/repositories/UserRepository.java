4d66677d4fbb7695e67df2d640b74572cf9bd7e0
João Lucas <joaolucasdeoliveiralima2@gmail.com>
2024-08-05 22:13:57 -0300

    feat: adding findByUsername method in UserRepository via derivative query

--- src/main/java/br/com/escritorioDeVaquejada/vqr/repositories/UserRepository.java
+++ src/main/java/br/com/escritorioDeVaquejada/vqr/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package br.com.escritorioDeVaquejada.vqr.repositories;
 
 import br.com.escritorioDeVaquejada.vqr.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
 }
